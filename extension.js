474fae2ae3f66da91cc88dad7e1d4a109a2dbe4e
Cold-Mint <cold_mint@qq.com>
2025-11-24 17:51:51 +0800

    add MPRIS media playback monitoring and remove panel button interactivity.

--- extension.js
+++ extension.js
@@ -20,9 +20,115 @@ import GObject from 'gi://GObject';
 import St from 'gi://St';
 import Clutter from 'gi://Clutter';
 import GLib from 'gi://GLib';
+import Gio from 'gi://Gio';
 
 import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import { MprisSource } from 'resource:///org/gnome/shell/ui/mpris.js';
+
+class MediaMonitor {
+    constructor(onUpdate) {
+        this._source = null;
+        this._players = new Map();
+        this._onUpdate = onUpdate;
+    }
+
+    enable() {
+        this._source = new MprisSource();
+        this._source.connect('player-added', (source, player) => {
+            this._addPlayer(player);
+        });
+        this._source.connect('player-removed', (source, player) => {
+            this._removePlayer(player);
+        });
+
+        for (const player of this._source.players) {
+            this._addPlayer(player);
+        }
+    }
+
+    disable() {
+        if (this._source) {
+            this._source = null;
+        }
+
+        for (const [player, signalId] of this._players) {
+            player.disconnect(signalId);
+        }
+        this._players.clear();
+    }
+
+    _addPlayer(player) {
+        if (this._players.has(player)) return;
+
+        const signalId = player.connect('changed', () => {
+            this._handlePlayerChange(player);
+        });
+        this._players.set(player, signalId);
+
+        // Check initial state
+        this._handlePlayerChange(player);
+    }
+
+    _removePlayer(player) {
+        if (this._players.has(player)) {
+            const signalId = this._players.get(player);
+            player.disconnect(signalId);
+            this._players.delete(player);
+        }
+    }
+
+    _handlePlayerChange(player) {
+        if (player.status === 'Playing') {
+            const title = player.trackTitle || 'Unknown Title';
+            this._onUpdate(title);
+        } else if (player.status === 'Paused') {
+            // We might want to check if other players are playing before clearing
+            // For now, simple logic: if this player paused, clear.
+            // But if we have multiple players, this might be racey.
+            // Let's iterate all players to see if any is playing.
+
+            let anyPlaying = false;
+            let playingTitle = '';
+
+            if (this._source) {
+                for (const p of this._source.players) {
+                    if (p.status === 'Playing') {
+                        anyPlaying = true;
+                        playingTitle = p.trackTitle || 'Unknown Title';
+                        break;
+                    }
+                }
+            }
+
+            if (anyPlaying) {
+                this._onUpdate(playingTitle);
+            } else {
+                this._onUpdate('');
+            }
+        } else {
+            // For Stopped or other states, also check if others are playing
+            let anyPlaying = false;
+            let playingTitle = '';
+
+            if (this._source) {
+                for (const p of this._source.players) {
+                    if (p.status === 'Playing') {
+                        anyPlaying = true;
+                        playingTitle = p.trackTitle || 'Unknown Title';
+                        break;
+                    }
+                }
+            }
+
+            if (anyPlaying) {
+                this._onUpdate(playingTitle);
+            } else {
+                this._onUpdate('');
+            }
+        }
+    }
+}
 
 export default class LyriTopExtension extends Extension {
     enable() {
@@ -32,6 +138,13 @@ export default class LyriTopExtension extends Extension {
 
         this._createIndicator();
         this._updatePosition();
+
+        this._mediaMonitor = new MediaMonitor((text) => {
+            if (this._label) {
+                this._label.text = text;
+            }
+        });
+        this._mediaMonitor.enable();
 
         this._settingsChangedId = this._settings.connect('changed::position', () => {
             this._updatePosition();
@@ -53,6 +166,11 @@ export default class LyriTopExtension extends Extension {
             this._offsetChangedId = null;
         }
 
+        if (this._mediaMonitor) {
+            this._mediaMonitor.disable();
+            this._mediaMonitor = null;
+        }
+
         this._destroyIndicator();
         this._settings = null;
     }
@@ -60,8 +178,6 @@ export default class LyriTopExtension extends Extension {
     _createIndicator() {
         this._box = new St.BoxLayout({
             style_class: 'panel-button',
-            reactive: true,
-            track_hover: true,
         });
 
         this._label = new St.Label({
@@ -71,18 +187,10 @@ export default class LyriTopExtension extends Extension {
 
         this._box.add_child(this._label);
 
-        this._clickId = this._box.connect('button-press-event', () => {
-            this.openPreferences();
-            return Clutter.EVENT_PROPAGATE;
-        });
     }
 
     _destroyIndicator() {
         if (this._box) {
-            if (this._clickId) {
-                this._box.disconnect(this._clickId);
-                this._clickId = null;
-            }
 
             if (this._box.get_parent()) {
                 this._box.get_parent().remove_child(this._box);
